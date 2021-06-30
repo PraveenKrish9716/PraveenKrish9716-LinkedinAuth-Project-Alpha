ce5950e90ef1db2f5498e8e745f857e3fba45f7d
PraveenKrish9716 <praveenbiber007@gmail.com>
2021-06-30 20:49:01 +0530

    Linkedin auth code

---
+++ Linkdin Auth/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'linkedinAuth': {
+      'clientID': '86q8czzy4de81z', // your App ID
+      'clientSecret': 'MAkDC6DTdALy0cuq', // your App Secret
+      'callbackURL': 'http://127.0.0.1:3000/auth/linkedin/callback'
+    }
+  }
